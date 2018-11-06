27baf826e94aaa42a0c73f7d593591965a10a463
Andrew Spalato <aspa_id@hotmail.com>
2018-11-06 21:09:00 +1100

    Added volumetric lights and a couple of scripts.

---
+++ GameDevArt/Assets/Scripts/Debug/FPSCounter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FPSCounter : MonoBehaviour 
+{
+	public static FPSCounter Instance { get; private set; }
+
+	[Tooltip ("How often to update calculating fps.")]
+	public float frequency = 0.5f;
+	public TextMeshProUGUI FPSText;
+	public bool showPrefix;
+	public int FramesPerSec { get; protected set; }
+	public float FramesPerSecB { get; protected set; }
+	public bool toggle;
+	public bool showFps;
+	private float avFps;
+	public float averageFps;
+
+	void Awake ()
+	{
+		Instance = this;
+	}
+
+	private void Start()
+	{
+		StartCoroutine (FPS ());
+
+		if (showFps == true)
+		{
+			FPSText.enabled = true;
+		}
+
+		if (showFps == false)
+		{
+			FPSText.enabled = false;
+		}
+	}
+
+	void Update ()
+	{
+		avFps += (Time.unscaledDeltaTime - avFps) * Time.unscaledDeltaTime;
+		averageFps = 1 / avFps;
+	}
+
+	private IEnumerator FPS () 
+	{
+		for(;;)
+		{
+			// Capture current framerate.
+			int lastFrameCount = Time.frameCount;
+
+			float lastTime = Time.realtimeSinceStartup; // Get time since startup.
+
+			// Sets how fast this updates.
+			yield return new WaitForSeconds (frequency);
+
+			// Get time span and frame count.
+			float timeSpan = Time.realtimeSinceStartup - lastTime;
+			int frameCount = Time.frameCount - lastFrameCount;
+			
+			// Display it.
+			FramesPerSec = Mathf.RoundToInt(frameCount / timeSpan);
+			FramesPerSecB = frameCount / timeSpan;
+			FPSText.text = FramesPerSec.ToString() + "";
+
+			if (showPrefix == true)
+			{
+				FPSText.text = "FPS: " + FramesPerSec.ToString() + " (" + ((1.0f/(FramesPerSecB)) * 1000.0f) + " ms)";
+			}
+		}
+	}
+}
---
+++ GameDevArt/Assets/Scripts/Debug/TargetFPS.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class TargetFPS : MonoBehaviour 
+{
+	public static TargetFPS Instance { get; private set; }
+
+	[Tooltip ("Game will try to render as fast as the target. Will try to run as fast as possible if set to a negative number.")]
+	public int targetFramerate = 60; 
+	public int currentTargetFramerate;
+	[Tooltip ("Use screen refresh rate and match framerate.")]
+	public bool useScreenRefreshRate;
+
+	void Awake ()
+	{
+		Instance = this;
+
+		if (useScreenRefreshRate == true) 
+		{
+			Application.targetFrameRate = Screen.currentResolution.refreshRate;
+		} 
+
+		InvokeRepeating ("UpdateCurrentTargetFramerate", 0, 1);
+	}
+
+	void UpdateCurrentTargetFramerate ()
+	{
+		currentTargetFramerate = Application.targetFrameRate;
+	}
+
+	// Set target framerate by other scripts.
+	public void SetTargetFramerate (int framerate)
+	{
+		Application.targetFrameRate = framerate; 
+		//SaveAndLoadScript.Instance.targetframerate = framerate;
+		//Debug.Log ("Target framerate set to " + framerate + " FPS.");
+	}
+}
