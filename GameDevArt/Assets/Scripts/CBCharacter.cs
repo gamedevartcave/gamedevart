aef26bbe0be69347f636fa3f9ce0667d39b1e066
Jacques Van Niekerk <jshark46@gmail.com>
2018-11-28 13:51:38 +1100

    Intil Statemachine

---
+++ GameDevArt/Assets/Scripts/CBCharacter.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CityBashers
+{
+    public enum CharacterStates
+    {
+        Unknown,
+        Idle,
+        Movement,
+        Paused,
+        Interacting,
+        Death
+    }
+
+    public enum MovementStates
+    {
+        Unknown,
+        Idle,
+        Walking,
+        Running,
+        Jumping,
+        Falling,
+        KnockedDown,
+        GetingUp,
+        Dashing,
+        Dogding
+    }
+    public enum CombatStates
+    {
+        Unknown,
+        Idle,
+        ChangingWeapon,
+        Reloading,
+        MeleeAttack,
+        FireWeapon,
+        Blocking,
+        Parry
+    }
+    public class CBCharacter : MonoBehaviour
+    {
+        private StateMachine MovementState;
+        private StateMachine CombatState;
+        private StateMachine CharacterStates;
+
+        // Use this for initialization
+        void Start()
+        {
+            MovementState = new StateMachine();
+            CombatState = new StateMachine();
+            CharacterStates = new StateMachine();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        MovementState.Update();
+        CombatState.Update();
+        CharacterStates.Update();
+        }
+        void FixedUpdate()
+        {
+            MovementState.FixedUpdate();
+            CombatState.FixedUpdate();
+            CharacterStates.FixedUpdate();
+        }
+    }
+}
---
+++ GameDevArt/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CityBashers
+{
+    public abstract class StateBehaviour
+    {
+        public PlayerController PlayerController { get; private set; }
+
+        public StateBehaviour(PlayerController controller)
+        {
+            PlayerController = controller;
+        }
+
+        protected Transform transform
+        {
+            get { return PlayerController.transform; }
+        }
+
+        protected static T Instantiate<T>(T original, Transform parent) where T : UnityEngine.Object
+        {
+            return UnityEngine.Object.Instantiate<T>(original, parent, false);
+        }
+
+        public virtual void OnStateEnter(StateMachine stateMachine) { }
+        public virtual void OnStateUpdate(StateMachine stateMachine) { }
+        public virtual void OnStateFixedUpdate(StateMachine stateMachine) { }
+        public virtual void OnStateExit(StateMachine stateMachine) { }
+        public virtual void OnBackgroundUpdate() { }
+
+        public virtual void OnTriggerEnter(Collider other) { }
+        public virtual void OnTriggerStay(Collider other) { }
+        public virtual void OnCollisionEnter(Collision other) { }
+        public virtual void OnCollisionStay(Collision other) { }
+        public virtual void OnCollisionExit(Collision other) { }
+    }
+
+    public class StateMachine
+    {
+        public StateBehaviour PreviousState { get; private set; }
+        public StateBehaviour CurrentState { get; private set; }
+        private UnityEvent StatesBackgroundUpdate;
+
+        public void Update()
+        {
+            CurrentState.OnStateUpdate(this);
+            StatesBackgroundUpdate.Invoke();
+        }
+
+        public void FixedUpdate()
+        {
+            CurrentState.OnStateFixedUpdate(this);
+        }
+
+        public void ChangeState(StateBehaviour newState)
+        {
+            PreviousState = CurrentState;
+            CurrentState.OnStateExit(this);
+            CurrentState = newState;
+            CurrentState.OnStateEnter(this);
+        }
+    }
+}
