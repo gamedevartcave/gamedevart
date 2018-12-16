6679e4107669b4d3827e0e7468916b292d637296
Andrew Spalato <7651775@student.swin.edu.au>
2018-12-16 15:51:54 +1100

    Added ability to move sliders by key press.

---
+++ GameDevArt/Assets/Scripts/Input/SliderInput.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using InControl;
+
+namespace CityBashers
+{
+	public class SliderInput : MonoBehaviour 
+	{
+		[ReadOnlyAttribute] public bool active;
+		public float moveAmount = 1;
+		[Space (10)]
+		public MenuNavigation menuNav;
+		[Space (10)]
+		public Slider slider;
+		public Selectable selectable;
+
+		public PlayerActions playerActions;
+
+		void Awake ()
+		{
+			DontDestroyOnLoadInit.Instance.OnInitialize.AddListener (OnInitialize);
+		}
+
+		void OnInitialize ()
+		{
+			FetchComponents ();
+		}
+
+		void FetchComponents ()
+		{
+			if (playerActions == null) playerActions = InControlActions.instance.playerActions;
+		}
+
+		void Update ()
+		{
+			if (menuNav.isActiveMenu == true)
+			{
+				if (menuNav.currentSelectable == selectable)
+				{
+					if (active == false)
+					{
+						active = true;
+					}
+				
+					if (playerActions.Move.Value.x != 0)
+					{
+						slider.value += moveAmount * playerActions.Move.Value.x;
+					}
+				} 
+
+				else
+				
+				{
+					if (active == true)
+					{
+						active = false;
+					}
+				}
+			}
+		}
+	}
+}
